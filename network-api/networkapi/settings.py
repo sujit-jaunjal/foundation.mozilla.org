94a914aa93fab0e068bd4b2597b67354f0069a89
Mavis Ou <mmmavis@users.noreply.github.com>
2021-02-09 15:49:56 -0800

    (WIP) redirect /pt/* to /pt-BR/*

--- network-api/networkapi/settings.py
+++ network-api/networkapi/settings.py
@@ -411,7 +411,7 @@ LANGUAGE_CODE = 'en'
 LANGUAGES = (
     ('en', gettext_lazy('English')),
     ('de', gettext_lazy('German')),
-    ('pt', gettext_lazy('Portuguese')),
+    ('pt-BR', gettext_lazy('Portuguese')),
     ('es', gettext_lazy('Spanish')),
     ('fr', gettext_lazy('French')),
     ('fy-NL', gettext_lazy('Frisian')),
